d518dbbf143886911c8bde0f6cf2af76a079ace3
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2026-02-04 12:28:06 +0530

    Change to google from generativeai

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-import chainlit as cl
-import google.generativeai as genai
-from fastmcp import FastMCP
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-genai.configure(api_key = os.getenv("GENAI_API_KEY"))
+import chainlit as cl
+from google import genai
+from dotenv import  load_dotenv
+from proto.marshal.compat import message
 
 from server import read_employees, update_employee_record
 
-model = genai.GenerativeModel(
-    model_name = "gemini-1.5-flash",
-    tools = [read_employees, update_employee_record]
-)
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+MODEL_ID = "gemini-1.5-flash"
 
 @cl.on_chat_start
 async def start():
-    cl.user_session.set("chat_session", model.start_chat(history=[]))
-    await cl.Message(content = "Hi! I'm your AI assistant. How can I help you today?").send()
+    cl.user_session.set("chat_history", [])
+    await cl.Message(content="System Ready. I can read and update employees.md!").send()
 
 @cl.on_message
 async def main(message: cl.Message):
-    chat_session = cl.user_session.get("chat_session")
+    history = cl.user_session.get("chat_history")
 
-    response = chat_session.send_message(message.content)
-    await cl.Message(content = response.text).send()
+    response = client.models.generate_content(
+        model = MODEL_ID,
+        content = message.content,
+        config={
+            "tools" : [read_employees, update_employee_record],
+        }
+    )
+
+    history.append({"role": "user", "parts": [{"text": message.content}]})
+    history.append({"role": "model", "parts": [{"text": response.text}]})
+    await cl.Message(content=response.text).send()
